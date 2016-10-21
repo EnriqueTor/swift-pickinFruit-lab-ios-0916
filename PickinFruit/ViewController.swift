74288b0c57bc05a5437e2d1caa3a2e50bea2719b
EnriqueTor <etorrendell@gmail.com>
2016-10-20 21:06:08 -0400

    Complete banana

--- PickinFruit/ViewController.swift
+++ PickinFruit/ViewController.swift
@@ -8,26 +8,67 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
     @IBOutlet weak var spinButton: UIButton!
     @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var fruitPicker: UIPickerView!
     
     var fruitsArray = ["🍎", "🍊", "🍌", "🍐", "🍇", "🍉", "🍓", "🍒", "🍍"]
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         
         self.spinButton.accessibilityLabel = Constants.SPIN_BUTTON
         
+        fruitPicker.delegate = self
+        fruitPicker.dataSource = self
     }
     
     @IBAction func spinButtonTapped(_ sender: UIButton) {
         
+        var fruits: [Int] = []
         
+        for fruit in 0..<3 {
+            
+            let number = Int(arc4random_uniform(UInt32(fruitsArray.count)))
+            fruitPicker.selectRow(number, inComponent: fruit, animated: true)
+            
+            fruits.append(number)
+        }
+        
+        testForWin(fruits: fruits)
         
     }
-
+    
+    func testForWin(fruits: [Int]) {
+        
+        if fruits[0] == fruits[1] && fruits[1] == fruits[2] {
+            resultLabel.text = "WINNER!"
+        }
+        else {
+            resultLabel.text = "TRY AGAIN"
+        }
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 3
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return fruitsArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return fruitsArray[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        resultLabel.text = fruitsArray[row]
+    }
+    
+    
 }
 
 // MARK: Set Up
